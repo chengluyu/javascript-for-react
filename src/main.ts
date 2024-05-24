7f2e01383dbd1f12e0f370d7f2c3ad3ca4b85a52
Luyu Cheng <chengluyu@live.cn>
2024-05-25 02:07:56 +0800

    feat: polish example and page layout

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,50 @@ import "./index.css";
 import { EditorView, basicSetup } from "codemirror";
 import { javascript } from "./language";
 
-const example = `import * as React from 'react';
-
-component Introduction(name: string, age: number) {
-  return <h1>My name is {name} and I am {age} years old</h1>
+const example = `hook useCounter(initialValue?: number) {
+  state count = initialValue ?? 0;
+  const increment = () => mutate x => x + 1
+  const decrement = () => mutate x => x - 1
+  const reset = () => mutate x = initialValue ?? 0
+  return { count, increment, decrement, reset }
 }
 
-hook useMultiplier(x: number): number {
-  state y = 1;
-  useEffect(() => { mutate y = 0 })
-  return x * y;
-}
+hook useInterval(callback: () => void, delay: number | null) {
+  const savedCallback = useRef(callback)
 
-hook useChatRoom(roomId) {
-  const [serverUrl, setServerUrl] = useState('https://localhost:1234');
+  // Remember the latest callback if it changes.
+  useIsomorphicLayoutEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  // Set up the interval.
   effect {
-    const connection = createConnection(serverUrl, roomId);
-    connection.connect();
-    cleanup {
-      connection.disconnect();
-    }
+    // Don't schedule if no delay is specified.
+    // Note: 0 is a valid value for delay.
+    if (delay === null) break
+    const id = setInterval(() => {
+      savedCallback.current()
+    }, delay)
+    cleanup { clearInterval(id) }
   }
+}
+
+component Counter() {
+  const { count, increment, decrement, reset } = useCounter()
+  useInterval(reset, 5)
+  return (
+    <div>
+      <div>The current value is {count}</div>
+      <div className="flex gap-2">
+        <button onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+      </div>
+      <p>The counter will be reset in every 5 seconds.</p>
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  )
 }
 `
 
